9f77784164f6939d977ab5c8a5e1a190dcc30b82
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-07-10 13:55:13 +0200

    Projektdateien hinzufügen.

---
+++ CanBusSniffer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CanBusSniffer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CanBusSniffer/Models/CanFrame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanBusSniffer.Models
+{
+    internal class CanFrame
+    {
+        public int FrameID { get; set; }
+        public int DataLengthContent { get; set; }
+        public byte[] Data { get; set; } = new byte[8];
+
+        public CanFrame(int frameID, int dataLengthContent, byte[] data)
+        {
+            FrameID = frameID;
+            DataLengthContent = dataLengthContent;
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            string r = string.Empty;
+            r += ($"{FrameID:X}: ");
+            for (int i = 0; i < DataLengthContent; i++)
+            {
+                r += $"{Data[i]}";
+            }
+
+            r += "\n";
+
+            return r;
+        }
+    }
+}
