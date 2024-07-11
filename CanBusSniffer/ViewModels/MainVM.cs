03de2055f4cf9cd83e0f7f64f8e4e5705154ac7a
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-07-11 19:13:19 +0200

    Renamed

--- CanBusSniffer/ViewModels/MainVM.cs
+++ CanBusSniffer/ViewModels/MainVM.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,26 @@ using System.Windows.Input;
 
 namespace CanBusSniffer.ViewModels
 {
-    public partial class MainPageVM : ObservableObject
+    public partial class MainVM : ObservableObject
     {
         [ObservableProperty]
         private int num = 0;
 
-        public MainPageVM()
+        public MainVM()
         {
         }
 
         [RelayCommand]
         private async Task OpenBluetoothMenueAsync()
         {
-            Num++;
+            if (Shell.Current is not null)
+            {
+                await Shell.Current.GoToAsync(nameof(BluetoothPage));
+            }
+            else
+            {
+                Debug.WriteLine("No instance of shell");
+            }
         }
     }
 }
