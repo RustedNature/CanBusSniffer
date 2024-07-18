35ca3b18426f32e72be4bf538309fd7104442623
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-07-18 18:56:28 +0200

    Refactoring

--- CanBusSniffer/Models/CanFrame.cs
+++ CanBusSniffer/Models/CanFrame.cs
@@ -4,27 +4,37 @@ namespace CanBusSniffer.Models;
 
 public class CanFrame
 {
-    public CanFrame(uint frameID, int dataLengthContent, byte[] data)
+    private const uint MaxDataLength = 8;
+    public CanFrame(uint frameId, int dataLengthContent, List<byte> data)
     {
-        FrameID = frameID;
+        FrameId = frameId;
         DataLengthContent = dataLengthContent;
-        for (var i = 0; i < DataLengthContent; i++) Data[i] = data[i];
+        
+        for (var i = 0; i < MaxDataLength; i++)
+        {
+            if (i < DataLengthContent)
+            {
+              Data[i] = data[i];
+                
+            }
+            else
+            {
+                Data[i] = 0;
+            }
+        }
+        
     }
 
-    public CanFrame()
-    {
-    }
-
-    [JsonProperty("id")] public uint FrameID { get; set; }
+    [JsonProperty("id")] public uint FrameId { get; set; }
 
     [JsonProperty("dlc")] public int DataLengthContent { get; set; }
 
-    [JsonProperty("data")] public List<byte> Data { get; set; } = new();
+    [JsonProperty("data")] public byte[] Data { get; set; } = new byte[8];
 
     public override string ToString()
     {
         var r = string.Empty;
-        r += $"ID: 0x{FrameID:X} Data: ";
+        r += $"ID: 0x{FrameId:X} Data: ";
         for (var i = 0; i < DataLengthContent; i++) r += $"{Data[i]:X} ";
 
         return r;
