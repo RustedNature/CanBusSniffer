d1c69d1a5df91b691700e3f3099e45c3412244a9
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-07-26 18:25:42 +0200

    Updated for Equatable checks

--- CanBusSniffer/Models/CanFrame.cs
+++ CanBusSniffer/Models/CanFrame.cs
@@ -2,34 +2,34 @@
 
 namespace CanBusSniffer.Models;
 
-public class CanFrame
+public class CanFrame : IEquatable<CanFrame>
 {
     private const uint MaxDataLength = 8;
+
     public CanFrame(uint frameId, int dataLengthContent, List<byte> data)
     {
         FrameId = frameId;
         DataLengthContent = dataLengthContent;
-        
+
         for (var i = 0; i < MaxDataLength; i++)
         {
             if (i < DataLengthContent)
             {
-              Data[i] = data[i];
-                
+                Data[i] = data[i];
             }
             else
             {
                 Data[i] = 0;
             }
         }
-        
     }
 
-    [JsonProperty("id")] public uint FrameId { get; set; }
+    [JsonProperty("id")] public uint FrameId { get; }
 
     [JsonProperty("dlc")] public int DataLengthContent { get; set; }
 
     [JsonProperty("data")] public byte[] Data { get; set; } = new byte[8];
+
 
     public override string ToString()
     {
@@ -38,5 +38,26 @@ public class CanFrame
         for (var i = 0; i < DataLengthContent; i++) r += $"{Data[i]:X} ";
 
         return r;
+    }
+
+
+    public bool Equals(CanFrame? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return FrameId == other.FrameId;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((CanFrame)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return (int)FrameId;
     }
 }
